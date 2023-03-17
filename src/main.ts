373ce7554bd82ec23c22c36502234c6aab6c833c
JRamos <jplramos1993@gmail.com>
2023-03-17 12:39:49 +0000

    Favicon.

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import * as hbs from 'hbs';
+import * as favicon from 'serve-favicon';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
 
+  app.use(favicon(join(__dirname, '..', 'public', 'favicon.ico')))
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   hbs.registerPartials(join(__dirname, '..', 'views', 'partials'));
